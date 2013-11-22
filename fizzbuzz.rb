512b919fc9a068e631c660ed3834f8ce53459620
Gianni Guitteaud <gianni.guitteaud@gmail.com>
2013-11-22 12:44:48 +0000

    Clean Simple version

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,4 @@
 #Simple Solution
-
 class FizzBuzz
 	def self.fizz(number)
     return "FizzBuzz" if number % 15 == 0
@@ -9,12 +8,3 @@ class FizzBuzz
 	end
 end
 
-=begin
-More advance Solution
-1.upto(100) do |n|
-  print "Fizz" if a = (n % 3).zero?
-  print "Buzz" if b = (n % 5).zero?
-  print n unless (a || b)
-  print "\n"
-end
-=end
