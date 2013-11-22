9792d02cc39d63ebe5c9000d72896988c5e1d709
Gianni Guitteaud <gianni.guitteaud@gmail.com>
2013-11-22 12:35:23 +0000

    Clean Advanced Version

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,5 @@
-#Simple Solution
 
-# class FizzBuzz
-# 	def self.fizz(number)
-#     return "FizzBuzz" if number % 15 == 0
-#     return "Fizz" if number % 3 == 0
-#     return "Buzz" if number % 5 == 0
-#     number 
-# 	end
-# end
-
-# =begin
-More advance Solution
+#More advance Solution
 1.upto(100) do |n|
   print "Fizz" if a = (n % 3).zero?
   print "Buzz" if b = (n % 5).zero?
